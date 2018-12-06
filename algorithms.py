1d592609c7e238e963900c8158c8be77e745710c
Renzhi Huang <renzhih@seas.upenn.edu>
2018-12-06 12:24:34 -0500

    Thompson

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def UCB(data, partial):
 	'''
@@ -15,17 +16,50 @@ def UCB(data, partial):
 		UCB = mu + np.sqrt(2*np.log(t)/n)
 		observed_arm = np.argmax(UCB)
 		y_t = data[observed_arm,t]
+
 		if(partial):
 			n[observed_arm] = n[observed_arm]+1
 			mu[observed_arm] = mu[observed_arm]+(y_t-mu[observed_arm])/n[observed_arm]
 		else:
 			n = n+1
 			mu = mu+(data[:,t]-mu)/n
+
 		if(y_t == 0):
 			regret[t] = regret[t-1]
 		else:
 			regret[t] = regret[t-1]+1
 	return regret
+
+
+def Thompson_sampling(data):
+	'''
+	data: 50 x 32657 numpy array
+	'''
+	m,T = data.shape
+	S = np.zeros(m)
+	F = np.zeros(m)
+	regret = np.zeros(T)
+	for t in range(1,T):
+		theta = np.random.beta(S+1,F+1)
+		i_t = np.argmax(theta)
+		r_t = data[i_t,t]
+		if(r_t==1):
+			S[i_t]=S[i_t]+1
+			regret[t] = regret[t-1]
+		else:
+			F[i_t]=F[i_t]+1
+			regret[t] = regret[t-1]+1
+	return regret
+
+
+
+
+
+
+
+
+
+
 
 
 
--- run.py
+++ run.py
@@ -4,15 +4,24 @@ import matplotlib.pyplot as plt
 
 # parameters to be set
 path = './yahoo_ad_clicks.csv'
-partial = False # True to be partial feedback, False to be full feedback
+partial = True # True to be partial feedback, False to be full feedback
 
 # data import
 data = data_import(path)
 print(type(data)) # numpy array
 print(data.shape) # 50 x 32657
 
+# gain some knowledge about the data
+m,T = data.shape
+mu = np.mean(data,axis = 1)
+best_choice = np.argmax(mu)
+highest_mean = np.max(mu)
+print(highest_mean)
+
+
 # test algorithms
-regret = UCB(data,partial)
+#regret = UCB(data,partial)
+regret = Thompson_sampling(data)
 print(regret)
 plt.plot(regret)
 plt.show()
