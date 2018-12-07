2ee37e2631c894f1262c79d18d51053baaf04792
Renzhi Huang <renzhih@seas.upenn.edu>
2018-12-06 20:53:14 -0500

    fixed UCB

--- algorithms.py
+++ algorithms.py
@@ -8,7 +8,7 @@ def UCB(data, partial):
 	'''
 	m,T = data.shape
 	# the mean
-	mu = data[:,0]
+	mu = data[:,0].astype(np.float)
 	# the test number 
 	n = np.ones(m)
 	regret = np.ones(T)
@@ -30,6 +30,34 @@ def UCB(data, partial):
 
 	return regret, regret_t, reward
 
+def UCB_pro(data, initial_rounds):
+	'''
+	data: 50 x 32657 numpy array
+	partial: boolean to indicate if it is partial feedback or full feedback
+	'''
+	m,T = data.shape
+	# the mean
+	mu = np.mean(data[:,0:initial_rounds],axis=1)
+	# the test number 
+	n = np.ones(m)
+	regret = np.ones(T)
+	regret_t = np.ones(T)
+	reward = np.zeros(T)
+	ads = [x for x in range(m)]
+	for t in range(initial_rounds,T):
+		UCB = mu + np.sqrt(2*np.log(t)/n)
+		UCB = UCB / np.sum(UCB)
+		#i_t = np.random.choice(ads,p=UCB)
+		i_t = np.argmax(UCB)
+		r_t = data[i_t,t]
+		reward[t] = reward[t-1]+r_t
+		regret[t] = regret[t-1]+ (np.max(mu)-mu[i_t])
+		regret_t[t] = regret[t]/t
+		n[i_t] = n[i_t]+1
+		mu[i_t] = mu[i_t]+(r_t-mu[i_t])/n[i_t]
+
+	return regret, regret_t, reward
+
 
 def Thompson_sampling(data,partial):
 	'''
@@ -48,7 +76,9 @@ def Thompson_sampling(data,partial):
 		r_t = data[i_t,t]
 		if(t>0):
 			reward[t] = reward[t-1]+r_t
+			untested = np.where(S+F==0)
 			mu = S/(S+F)
+			mu[untested] = 0
 			regret[t] = regret[t-1]+ (np.max(mu)-mu[i_t])
 			regret_t[t] = regret[t]/t
 		else:
