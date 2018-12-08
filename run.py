96dfe3069705220f1e149d29e1da98be1a6b1db5
GalaxyDirector <galaxydirector@MacBookAir.local>
2018-12-08 18:41:33 -0500

    automatic plotting and optimize graphs

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ def plot_(data, output_path, num_skip = 20, name = 'regret'):
 	plt.plot(data[num_skip:])
 	plt.xlabel('iteration')
 	plt.ylabel(name)
+	plt.title(name)
 	path = os.path.join(output_path, '{}.png'.format(name))
 	plt.savefig(path)
 	plt.close()
@@ -42,18 +43,19 @@ print(np.max(round_sum))
 # test algorithms
 # regret, regret_t,reward = epsilon_greedy(data, epsilon=None)
 # regret, regret_t,reward = UCB(data,partial,alpha)
-# regret, regret_t, reward = Thompson_sampling(data,partial)
+regret, regret_t, reward = Thompson_sampling(data,partial)
 
-for e in range(2,10,2):
-	regret, regret_t,reward = epsilon_greedy(data, epsilon=e/10)
-	output_path = './greedy_{}'.format(e)
-	plot_(reward, output_path, num_skip = 20, name = 'reward')
-	plot_(regret, output_path, num_skip = 20, name = 'regret')
-	plot_(regret_t, output_path, num_skip = 20, name = 'mean_regret')
+# for e in range(2,10,2):
+# 	regret, regret_t,reward = epsilon_greedy(data, epsilon=e/10)
+# 	output_path = './greedy_{}'.format(e)
+output_path = './Thompson'
+plot_(reward, output_path, num_skip = 20, name = 'reward')
+plot_(regret, output_path, num_skip = 20, name = 'regret')
+plot_(regret_t, output_path, num_skip = 20, name = 'mean_regret')
 
-	print(reward[-10:])
-	print(regret[-10:])
-	print(regret_t[-10:])
+print(reward[-10:])
+print(regret[-10:])
+print(regret_t[-10:])
 # plt.plot(regret_t[20:])
 # plt.show()
 
